8417972a1509a0ddec5b2b3cc4a1e91d85b950d7
Benjamin Levy <benjamin.levy@ll.mit.edu>
2024-07-23 11:03:32 -0400

    Unfinished: support custom ioctl modeling
    This commit tries to implement arbitrary, Turing-complete ioctl() models
    for pseudofiles. Linux doesn't support this with FUSE, so this commit
    uses the related CUSE API. Unfortunately, CUSE doesn't allow opened
    files to have an offset, so seek() doesn't work, and subsequent read()
    calls return the same data, which prevents e.g. cat from terminating
    when reading the file. If we ever want to support this feature, we will
    most likely need a kernel patch.
    
    https://elixir.bootlin.com/linux/v4.10/source/fs/fuse/cuse.c#L187

--- src/pkgs/hyperfs/hyperfs.c
+++ src/pkgs/hyperfs/hyperfs.c
@@ -45,6 +45,8 @@
 #define MAGIC_VALUE 0x51ec3692 // crc32("hyperfs")
 #define PACKED __attribute__((packed))
 
+#include <assert.h>
+#include <cuse_lowlevel.h>
 #include <dirent.h>
 #include <errno.h>
 #include <fuse.h>
@@ -55,12 +57,15 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/sysmacros.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include "hypercall.h"
 
 static size_t num_hyperfiles;
 static char **hyperfile_paths;
+static dev_t *hyperfile_dev_nums;
+static size_t cuse_hyperfile_index;
 
 static struct options { const char *passthrough_path; } options;
 
@@ -76,7 +81,7 @@ enum { HYP_FILE_OP, HYP_GET_NUM_HYPERFILES, HYP_GET_HYPERFILE_PATHS };
 
 enum { READ, WRITE, IOCTL, GETATTR };
 
-enum { DEV_MODE = S_IFREG | 0666, DIR_MODE = S_IFDIR | 0777 };
+enum { DEV_MODE = S_IFCHR | 0666, DIR_MODE = S_IFDIR | 0777 };
 
 enum { HYPERFILE_PATH_MAX = 1024 };
 
@@ -96,13 +101,19 @@ struct hyperfs_data {
     } PACKED write;
     struct {
       unsigned int cmd;
-      void *data;
+      void **ptr;
+      size_t *size;
+      int *rw;
     } PACKED ioctl;
     struct {
       off_t *size;
     } PACKED getattr;
   } PACKED;
 } PACKED;
+
+static size_t min(size_t x, size_t y) {
+  return x < y ? x : y;
+}
 
 static void page_in_hyperfs_data(struct hyperfs_data *data) {
   volatile unsigned char x = 0;
@@ -165,6 +176,7 @@ static int hyperfs_getattr(const char *path, struct stat *st,
     st->st_nlink = !strcmp(path, "/") ? 2 : 1;
     st->st_mode = mode;
     if (mode == DEV_MODE) {
+      st->st_rdev = hyperfile_dev_nums[cuse_hyperfile_index];
       hyp_file_op((struct hyperfs_data){
           .type = GETATTR,
           .path = path,
@@ -223,49 +235,76 @@ static int hyperfs_truncate(const char *path, off_t offset,
   }
 }
 
-static int hyperfs_read(const char *path, char *buf, size_t size, off_t offset,
-                        struct fuse_file_info *fi) {
+static void clop_read(fuse_req_t req, size_t size, off_t off, struct fuse_file_info *fi) {
   (void)fi;
 
-  return lookup_mode(path) == DEV_MODE ? hyp_file_op((struct hyperfs_data){
-                                             .type = READ,
-                                             .path = path,
-                                             .read.buf = buf,
-                                             .read.size = size,
-                                             .read.offset = offset,
-                                         })
-                                       : xmp_read(path, buf, size, offset, fi);
+  char buf[1024] = { 0 };
+
+  FILE *f = fopen("/dev/ttyAMA0", "w");
+  fprintf(f, "HYPERFS: read with offset %d\n", (int)off);
+  fflush(f);
+  fclose(f);
+
+  int nr = hyp_file_op((struct hyperfs_data){
+    .type = READ,
+    .path = hyperfile_paths[cuse_hyperfile_index],
+    .read.buf = buf,
+    .read.size = min(size, sizeof(buf)),
+    .read.offset = off,
+  });
+
+  assert(nr >= 0);
+
+  fuse_reply_buf(req, buf, nr);
 }
 
-static int hyperfs_write(const char *path, const char *buf, size_t size,
-                         off_t offset, struct fuse_file_info *fi) {
+static void clop_write(fuse_req_t req, const char *buf, size_t size,
+                       off_t off, struct fuse_file_info *fi) {
   (void)fi;
 
-  return lookup_mode(path) == DEV_MODE ? hyp_file_op((struct hyperfs_data){
-                                             .type = WRITE,
-                                             .path = path,
-                                             .write.buf = buf,
-                                             .write.size = size,
-                                             .write.offset = offset,
-                                         })
-                                       : xmp_write(path, buf, size, offset, fi);
+  int nr = hyp_file_op((struct hyperfs_data){
+    .type = WRITE,
+    .path = hyperfile_paths[cuse_hyperfile_index],
+    .write.buf = buf,
+    .write.size = size,
+    .write.offset = off,
+  });
+
+  fuse_reply_write(req, nr);
 }
 
-static int hyperfs_ioctl(const char *path, unsigned int cmd, void *arg,
-                         struct fuse_file_info *fi, unsigned int flags,
-                         void *data_) {
-  (void)arg;
-  (void)fi;
-  (void)flags;
+static void clop_ioctl(fuse_req_t req, int cmd, void *arg,
+                       struct fuse_file_info *fi, unsigned flags,
+                       const void *in_buf, size_t in_bufsz, size_t out_bufsz) {
+  (void) arg;
+  (void) fi;
+  (void) flags;
+  (void) in_buf;
+  (void) in_bufsz;
+  (void) out_bufsz;
 
-  return lookup_mode(path) == DEV_MODE
-             ? hyp_file_op((struct hyperfs_data){
-                   .type = IOCTL,
-                   .path = path,
-                   .ioctl.cmd = cmd,
-                   .ioctl.data = data_,
-               })
-             : xmp_ioctl(path, cmd, arg, fi, flags, data_);
+  void *ptr = NULL;
+  size_t size;
+  int rw = -1;
+  int result = hyp_file_op((struct hyperfs_data){
+    .type = IOCTL,
+    .path = hyperfile_paths[cuse_hyperfile_index],
+    .ioctl.cmd = cmd,
+    .ioctl.ptr = &ptr,
+    .ioctl.size = &size,
+    .ioctl.rw = &rw,
+  });
+  if (ptr) {
+    struct iovec iov = { ptr, size };
+    if (rw == READ) {
+      fuse_reply_ioctl_retry(req, NULL, 0, &iov, 1);
+    } else {
+      assert(rw == WRITE);
+      fuse_reply_ioctl_retry(req, &iov, 1, NULL, 0);
+    }
+  } else {
+    fuse_reply_ioctl(req, result, NULL, 0);
+  }
 }
 
 static int hyperfs_readlink(const char *path, char *buf, size_t size) {
@@ -287,19 +326,23 @@ static int hyperfs_release(const char *path, struct fuse_file_info *fi) {
   }
 }
 
+static void clop_open(fuse_req_t req, struct fuse_file_info *fi) {
+  fuse_reply_open(req, fi);
+}
+
 static const struct fuse_operations fops = {
     .open = hyperfs_open,
     .getattr = hyperfs_getattr,
     .readdir = hyperfs_readdir,
     .truncate = hyperfs_truncate,
-    .read = hyperfs_read,
-    .write = hyperfs_write,
-    .ioctl = hyperfs_ioctl,
 
     .readlink = hyperfs_readlink,
     .release = hyperfs_release,
 
     .init = xmp_init,
+    .read = xmp_read,
+    .write = xmp_write,
+    .ioctl = xmp_ioctl,
     .mknod = xmp_mknod,
     .mkdir = xmp_mkdir,
     .unlink = xmp_unlink,
@@ -314,6 +357,13 @@ static const struct fuse_operations fops = {
     .fsync = xmp_fsync,
 };
 
+static const struct cuse_lowlevel_ops clops = {
+    .open = clop_open,
+    .read = clop_read,
+    .write = clop_write,
+    .ioctl = clop_ioctl,
+};
+
 static void load_hyperfile_paths(void) {
   hc(HYP_GET_NUM_HYPERFILES, (void *[]){&num_hyperfiles}, 1);
   hyperfile_paths = calloc(num_hyperfiles, sizeof(*hyperfile_paths));
@@ -324,13 +374,64 @@ static void load_hyperfile_paths(void) {
 }
 
 int main(int argc, char *argv[]) {
+  // Parse command-line arguments
   struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
   if (fuse_opt_parse(&args, &options, option_spec, NULL)) {
-    return 1;
+    exit(EXIT_FAILURE);
   }
   if (!options.passthrough_path) {
     fputs("error: missing --passthrough-path\n", stderr);
+    exit(EXIT_FAILURE);
   }
+
   load_hyperfile_paths();
+
+  // Fork to make a process for each CUSE device, and save their device numbers
+  hyperfile_dev_nums = calloc(num_hyperfiles, sizeof(dev_t));
+  for (size_t i = 0; i < num_hyperfiles; i++) {
+    pid_t pid = fork();
+    switch (pid) {
+      case -1:
+        perror("fork");
+        exit(EXIT_FAILURE);
+      case 0: {
+        cuse_hyperfile_index = i;
+        char dev_name[HYPERFILE_PATH_MAX * 2];
+        int len = snprintf(
+          dev_name,
+          sizeof(dev_name),
+          "DEVNAME=hyperfs/%s",
+          hyperfile_paths[cuse_hyperfile_index]
+        );
+        if (len >= sizeof(dev_name)) {
+          perror("snprintf");
+          exit(EXIT_FAILURE);
+        }
+        struct cuse_info ci;
+        memset(&ci, 0, sizeof(ci));
+        ci.dev_major = i == 0 ? 0 : hyperfile_dev_nums[0]; // Auto-allocate device number
+        ci.dev_minor = i;
+        ci.dev_info_argc = 1;
+        ci.dev_info_argv = (const char *[]){ dev_name };
+        ci.flags = CUSE_UNRESTRICTED_IOCTL;
+        exit(cuse_lowlevel_main(args.argc, args.argv, &ci, &clops, NULL));
+      }
+    }
+    waitpid(pid, NULL, 0);
+    char dev_path[PATH_MAX];
+    int len = snprintf(dev_path, sizeof(dev_path), "/dev/hyperfs/%s", hyperfile_paths[i]);
+    if (len >= sizeof(dev_path)) {
+      perror("snprintf");
+      exit(EXIT_FAILURE);
+    }
+    struct stat st;
+    int err;
+    do {
+      err = stat(dev_path, &st);
+      usleep(10 * 1000); // 0.01 seconds
+    } while (err);
+    hyperfile_dev_nums[i] = st.st_rdev;
+  }
+ 
   return fuse_main(args.argc, args.argv, &fops, NULL);
 }
